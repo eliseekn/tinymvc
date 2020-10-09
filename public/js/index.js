233fbf8248c70690f36bbfcd42566f083ae4b6ac
N'Guessan Kouadio Elisée <eliseekn@gmail.com>
2020-10-09 09:03:49 +0000

    feat: update json package
    yarn can run multiple tasks with concurently command

--- public/js/index.js
+++ public/js/index.js
@@ -848,7 +848,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43243" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33801" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
