0cfa30b7592b017540af669db54ee5bbf508ba90
eliseekn <eliseekn@gmail.com>
2025-06-15 11:45:19 +0000

    refactor(code)

--- app/Database/Models/User.php
+++ app/Database/Models/User.php
@@ -57,8 +57,8 @@ class User extends Model
             ->select(['users.*', 'roles.name AS role'])
             ->join('roles', 'users.role_id', '=', 'roles.id')
             ->whereNotEquals('users.id', $userId)
-            ->subQueryWhen(! is_null($search), function (Repository $q) use ($search) {
-                $q->andRaw("(users.name LIKE '%$search%' OR email LIKE '%$search%')");
+            ->subQueryWhen(! is_null($search), function (Repository $r) use ($search) {
+                $r->andRaw("(users.name LIKE '%$search%' OR email LIKE '%$search%')");
             })
             ->orderDesc('users.created_at')
             ->paginate((int) $perPage, (int) $page);
