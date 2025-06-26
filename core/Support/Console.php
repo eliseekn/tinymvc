7774a5fe4eba05b47021bc26de6aa5fe6195eba3
eliseekn <eliseekn@gmail.com>
2025-06-26 15:36:51 +0000

    feat: add console command run support

---
+++ core/Support/Console.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
+ * @license MIT (https://opensource.org/licenses/MIT)
+ *
+ * @link https://github.com/eliseekn/tinymvc
+ */
+
+declare(strict_types=1);
+
+namespace Core\Support;
+
+use Symfony\Component\Process\Process;
+
+abstract class Console
+{
+    public static function run(string $command): void
+    {
+        $db = new Process(['php', 'console', $command]);
+        $db->setTimeout(null);
+        $db->disableOutput();
+        $db->run();
+    }
+}
