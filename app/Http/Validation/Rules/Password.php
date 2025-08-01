4f80a653b12a7b3b67c11339d4a2571856c930f0
eliseekn <eliseekn@gmail.com>
2025-08-01 18:25:12 +0000

    feat: add new rule

---
+++ app/Http/Validation/Rules/Password.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
+ * @license MIT (https://opensource.org/licenses/MIT)
+ *
+ * @link https://github.com/eliseekn/tinymvc
+ */
+
+declare(strict_types=1);
+
+namespace App\Http\Validation\Rules;
+
+use Core\Http\Validation\Rule\RuleInterface;
+use Somnambulist\Components\Validation\Rule;
+
+class Password extends Rule implements RuleInterface
+{
+    public ?string $name = 'passowrd';
+
+    public string $message = ':attribute must contains at least one uppercase letter, one lowercase letter and one digit';
+
+    public function check(mixed $value): bool
+    {
+        if (preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/', $value)) {
+            return true;
+
+        }
+
+        return false;
+    }
+}
