d4fcdb1a65c27f1ce200b067e0b121cee20055ac
eliseekn <eliseekn@gmail.com>
2026-04-16 20:14:59 +0000

    feat: add password update notification

---
+++ app/Notifications/Mails/PasswordUpdatedMail.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
+ * @license MIT (https://opensource.org/licenses/MIT)
+ *
+ * @link https://github.com/eliseekn/tinymvc
+ */
+
+declare(strict_types=1);
+
+namespace App\Notifications\Mails;
+
+use Core\Notification\Mail\Mail;
+
+class PasswordUpdatedMail extends Mail
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function send(?string $message = null): bool
+    {
+        return $this->mailer
+            ->from(config('notifications.mail.sender.email'), config('notifications.mail.sender.name'))
+            ->replyTo(config('notifications.mail.sender.email'), config('notifications.mail.sender.name'))
+            ->subject('Password updated')
+            ->html('emails.password_updated')
+            ->send();
+    }
+}
