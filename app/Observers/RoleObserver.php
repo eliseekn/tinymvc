083367208d7d898f119d2aa953c593b260b2048f
eliseekn <eliseekn@gmail.com>
2026-04-19 21:33:39 +0000

    feat: add role observer

---
+++ app/Observers/RoleObserver.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
+ * @license MIT (https://opensource.org/licenses/MIT)
+ *
+ * @link https://github.com/eliseekn/tinymvc
+ */
+
+declare(strict_types=1);
+
+namespace App\Observers;
+
+use Core\Cache\Cache;
+use Core\Database\Model;
+use Core\Observer\ObserverInterface;
+
+class RoleObserver implements ObserverInterface
+{
+    public static $table = 'roles';
+
+    public static function created(Model $model): void
+    {
+        Cache::forget(['roles', 'roles.dashboard']);
+    }
+
+    public static function updated(Model $model): void
+    {
+        Cache::forget(['roles', 'roles.dashboard']);
+    }
+
+    public static function deleted(Model $model): void
+    {
+        Cache::forget(['roles', 'roles.dashboard']);
+    }
+}
