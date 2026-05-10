ba6d0cc8efa409ffe7c46054f45d2f93e7ff9423
eliseekn <eliseekn@gmail.com>
2026-05-10 13:33:24 +0000

    feat: add new task

---
+++ app/Tasks/DeleteExpiredToken.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright 2019-2025 N'Guessan Kouadio Elisée <eliseekn@gmail.com>
+ * @license MIT (https://opensource.org/licenses/MIT)
+ *
+ * @link https://github.com/eliseekn/tinymvc
+ */
+
+declare(strict_types=1);
+
+namespace App\Tasks;
+
+use App\Database\Models\Token;
+use Core\Database\Model;
+use Core\Task\TaskInterface;
+
+final class DeleteExpiredToken implements TaskInterface
+{
+    public function handle(): void
+    {
+        Token::query()
+            ->select(['id', 'expires_at'])
+            ->chunk(100, function (Model $token) {
+                if (carbon($token->get('expires_at'))->addHour()->gte(carbon()->toDateTimeString())) {
+                    $token->delete();
+                }
+            });
+    }
+
+    public function handleCompleted(): void
+    {
+        //
+    }
+
+    public function handleFailed(): void
+    {
+        //
+    }
+}
