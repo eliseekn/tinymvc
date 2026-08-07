7d15e0786ce86ee8742af33eb0f6f843cb71ac80
eliseekn <eliseekn@gmail.com>
2026-08-07 11:48:05 +0000

    chore

--- core/Notification/Notification.php
+++ core/Notification/Notification.php
@@ -15,9 +15,7 @@ use PHPUnit\Framework\Assert;
 
 class Notification
 {
-    public function __construct(public NotificationInterface $notifiable)
-    {
-    }
+    public function __construct(public NotificationInterface $notifiable) {}
 
     public static function send(NotificationInterface $notifiable): self
     {
